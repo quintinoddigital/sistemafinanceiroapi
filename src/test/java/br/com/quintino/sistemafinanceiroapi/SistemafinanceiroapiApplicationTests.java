4fcc0cd2fee1ad40c696d38b4f37d4914dfb6bcc
Quintino Digital <quintino.digital@outlook.com.br>
2023-05-22 16:00:48 -0300

    :tada: init

---
+++ src/test/java/br/com/quintino/sistemafinanceiroapi/SistemafinanceiroapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.quintino.sistemafinanceiroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemafinanceiroapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
