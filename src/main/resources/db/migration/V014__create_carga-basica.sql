0eef17d3ee7e6bf43967299958ec9d4b43cb7fcc
Quintino Digital <quintino.digital@outlook.com.br>
2023-05-24 12:51:10 -0300

    :sparkles: feature: SISTEMAFINANCEIRO23052023151527
    https://github.com/quintinoddigital/sistemafinanceiroapi/issues/8

---
+++ src/main/resources/db/migration/V014__create_carga-basica.sql
@@ -0,0 +1,14 @@
+INSERT INTO TB_TIPO_PESSOA (DESCRICAO) values ('Pessoa Física');
+INSERT INTO TB_TIPO_PESSOA (DESCRICAO) values ('Pessoa Jurídica');
+
+INSERT INTO TB_PESSOA (ID_TIPO_PESSOA, NOME) values (1, 'José Quintino');
+INSERT INTO TB_PESSOA (ID_TIPO_PESSOA, NOME) values (2, 'Banco do Brasil');
+INSERT INTO TB_PESSOA (ID_TIPO_PESSOA, NOME) values (2, 'Amazon Brasil');
+
+INSERT INTO TB_TIPO_LANCAMENTO (IDENTIFICADOR, DESCRICAO) VALUES
+	('RECEITA_FIXA', 'Receita Fixa'),
+	('RECEITA_VARIAVEL', 'Receita Variável'),
+	('DESPESA_FIXA', 'Despesa Fixa'),
+	('DESPESA_VARIAVEL', 'Despesa Variável'),
+	('EMPRESTIMO_CONCESSAO','Empréstimo'),
+	('EMPRESTIMO_ADQUIRICAO','Empréstimo');
