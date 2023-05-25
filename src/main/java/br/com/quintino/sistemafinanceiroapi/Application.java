f6a81cff5fe30f828740213f5b72ac20d3ca401d
Quintinnno Digital <quintino.digital@outlook.com.br>
2023-05-24 22:09:20 -0300

    :sparkles: feature: SISTEMAFINANCEIRO24052023163932
    https://github.com/quintinoddigital/sistemafinanceiroapi/issues/9

--- src/main/java/br/com/quintino/sistemafinanceiroapi/Application.java
+++ src/main/java/br/com/quintino/sistemafinanceiroapi/Application.java
@@ -1,5 +1,6 @@
 package br.com.quintino.sistemafinanceiroapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @RequestMapping("/")
-public class Application {
+public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -18,6 +19,11 @@ public class Application {
 	@GetMapping
 	public String application() {
 		return "Sistema Financeiro (API)";
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("A API do Sistema Financeiro está em Execução (Versão 1.0.0)");
 	}
 
 }
